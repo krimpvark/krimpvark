263af5a8cbf049f9c99d311f967283994fe90e2e
Jacques Bronkhorst <jacquesb@agilebridge.co.za>
2021-12-03 11:05:17 +0200

    BDD Test Feature Added for interist Rate

---
+++ src/BDD/Feature/TestFeature.feature
@@ -0,0 +1,13 @@
+﻿Feature: InteristRate
+
+![InteristRate]
+Simple Calculation of Interist Rate
+
+@InteristRate
+Scenario: Calculate Interist Rate
+	Given the opening balance is 100
+	And the rate is 10
+	And the startDate is 2021-05-01
+	And the endDate is 2021-05-15
+	When the Interist rate is calculated
+	Then the result should be 417.7248169415651
---
+++ src/BDD/Steps/Steps.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TDD_Katas.BDD
+{
+    [Binding]
+    public sealed class StepsDefinition
+    {
+        private readonly InterestCalculator _interestCalculator;
+        private decimal _balance;
+        private decimal _rate;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private decimal _result;
+        public StepsDefinition(InterestCalculator interestCalculator)
+        {
+            _interestCalculator = interestCalculator;
+        }
+
+        [Given("the opening balance is (.*)")]
+        public void GivenBalance(decimal balance)
+        {
+            _balance = balance;
+        }
+
+        [Given("the rate is (.*)")]
+        public void GivenRate(decimal rate)
+        {
+            _rate = rate;
+        }
+
+        [Given("the startDate is (.*)")]
+        public void GivenStartDate(DateTime startDate)
+        {
+           _startDate = startDate;
+        }
+
+        [Given("the endDate is (.*)")]
+        public void GivenEndDate(DateTime endDate)
+        {
+            _endDate = endDate;
+        }
+
+        [When("the Interist rate is calculated")]
+        public void CalculateResult()
+        {
+          _result = _interestCalculator.Interestreceived(_balance, _rate, _startDate, _endDate);
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(decimal result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
